f82fad492743bb85feee9b802317a01cb8f10a64
Anita Ganti <65381336+anitapeppercorn@users.noreply.github.com>
2020-06-08 22:14:35 -0700

    Password Generator Project Files

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const characterAmountRange = document.getElementById('characterAmountRange')
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay')
+// Assignment code here
 
+// assign names to arrays for the character codes
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
@@ -17,33 +12,47 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
   arrayFromLowToHigh(123, 126)
 )
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
+// function to generate password, no arguments to pass
+function generatePassword() {
+// first prompt to ask user to enter length between 8 and 128
+  characterAmount = parseInt(
+    prompt(
+      "Welcome. You will now be asked a series of prompts. Please enter the length between 8 and 128 characters"
+    )
+  );
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  const characterAmount = characterAmountNumber.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeNumbers = includeNumbersElement.checked
-  const includeSymbols = includeSymbolsElement.checked
-  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-  passwordDisplay.innerText = password
-})
+  // conditional for Not a Number, <8 and >128 characters
+  if (isNaN(characterAmount) || characterAmount < 8 || characterAmount > 128) {
+    characterAmount = parseInt;
+    prompt("Oops! Please enter a password between 8 and 128 characters");
+  } else {
+    includeLowercase = confirm("Would you like lower case in your password?");
+    includeUppercase = confirm("Would you like upper case in your password?");
+    includeNumbers = confirm("Would you like numbers in your password?");
+    includeSymbols = confirm("Would you like special case in your password?");
+  }
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+// randrom number generator
+// defining the function to generate password
+// by default lowercase characters are included here. if other character types 
+// are included their character codes are concatenated to the total array
   let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  if (includeUppercase === true ) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols === true) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers=== true) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   
+// we define the const passwordCharacters as an array
   const passwordCharacters = []
+// we start a loop that increments for the number of password characters
   for (let i = 0; i < characterAmount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+// Push method adds new items to the end of the array and returns the new length
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join('')
 }
 
+//Definiong a function that creates an array with the character codes
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -52,8 +61,16 @@ function arrayFromLowToHigh(low, high) {
   return array
 }
 
-function syncCharacterAmount(e) {
-  const value = e.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
